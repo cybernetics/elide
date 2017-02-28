5f9d18d2027a6471a147bc80ae4186903e250506
klish <aklish@gmail.com>
2017-02-28 15:49:41 -0600

    Added enum support

--- elide-core/src/test/java/example/Author.java
+++ elide-core/src/test/java/example/Author.java
@@ -10,15 +10,14 @@ import com.yahoo.elide.annotation.Include;
 import com.yahoo.elide.annotation.SharePermission;
 import com.yahoo.elide.security.checks.prefab.Role;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Model for authors.
@@ -32,9 +31,16 @@ import javax.persistence.Table;
         logStatement = "{0}",
         logExpressions = {"${author.name}"})
 public class Author {
+    public enum AuthorType {
+        EXCLUSIVE,
+        CONTRACTED,
+        FREELANCE
+    }
+
     private Long id;
     private String name;
     private Collection<Book> books = new ArrayList<>();
+    private AuthorType type;
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
@@ -51,6 +57,14 @@ public class Author {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public AuthorType getType() {
+        return type;
+    }
+
+    public void setType(AuthorType type) {
+        this.type = type;
     }
 
     @ManyToMany(mappedBy = "authors")
